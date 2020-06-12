dd6bb6d370c1a4d7ae99195aac28c2f8dbdac79b
abiramen <7523422+abiramen@users.noreply.github.com>
2020-06-12 17:32:53 +1000

    Magically update to Vuetify 2 with no issues

--- nuxt/.nuxt/App.js
+++ nuxt/.nuxt/App.js
@@ -11,8 +11,6 @@ import {
 import NuxtError from '../layouts/error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
 
-import '../node_modules/vuetify/dist/vuetify.css'
-
 import _6f6c098b from '../layouts/default.vue'
 
 const layouts = { "_default": sanitizeComponent(_6f6c098b) }
